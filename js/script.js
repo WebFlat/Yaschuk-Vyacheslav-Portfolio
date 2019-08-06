0a69ba9e4181b8bfc932cc545724ebbf1f4a120e
WebFlat <y.vacheslav@gmail.com>
2019-08-07 01:09:27 +0300

    upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+   
+    
+//Animate Skills*********************************************
+    var shape = document.getElementById('progress');
+	var percentNumber = document.querySelector('.percentHtml');
+	let drowSkills = () => {
+    var shapeLength = (shape.getTotalLength().toFixed(0))-(shape.getTotalLength().toFixed(0))*(percentNumber.getAttribute('number'))/100;
+    var progressShow = shape.setAttribute('stroke-dashoffset', shapeLength);
+    	percentNumber.innerText = percentNumber.getAttribute('number') + '%';
+	};
+	drowSkills();
+
+
+	var shape = document.getElementById('progress_2');
+	var percentNumber = document.querySelector('.percentCss');
+	drowSkills();
+
+	var shape = document.getElementById('progress_3');
+	var percentNumber = document.querySelector('.percentJS');
+	drowSkills();
+
+	var shape = document.getElementById('progress_4');
+	var percentNumber = document.querySelector('.percentGulp');
+	drowSkills();
+
+	var shape = document.getElementById('progress_5');
+	var percentNumber = document.querySelector('.percentBootstrap');
+	drowSkills();
+
+	var shape = document.getElementById('progress_6');
+	var percentNumber = document.querySelector('.percentAdaptive');
+	drowSkills();
+
+//Animate 
+
+});
